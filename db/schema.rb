a0b69c3acd500bbd5ba714cb7a96f45c46cbe813
civnag <lucascn@usp.br>
2019-05-16 17:51:01 -0300

    Add logout functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_205412) do
+ActiveRecord::Schema.define(version: 2019_05_16_192411) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 2019_05_15_205412) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "full_name"
-    t.string "username"
-    t.string "password"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
